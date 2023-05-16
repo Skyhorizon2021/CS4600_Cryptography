d37cb842fd15a6e50ce6dc9b81e1185a8888df8d
Skyhorizon2021 <locnguyen02152021@gmail.com>
2023-05-16 15:22:52 -0700

    Set up Key Generator

---
+++ Final Project - Public Key/serverKeyGenerator.py
@@ -0,0 +1,9 @@
+import rsa
+
+publicKey, privateKey = rsa.newkeys(1024)
+
+with open("serverPublicKey.pem","wb") as file:
+    file.write(publicKey.save_pkcs1("PEM"))
+
+with open("serverPrivateKey.pem","wb") as file:
+    file.write(privateKey.save_pkcs1("PEM"))
