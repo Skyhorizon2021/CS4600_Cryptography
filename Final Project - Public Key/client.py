e41aaf3e98be71fd5aefa422c8863becd2aa3bd4
Skyhorizon2021 <locnguyen02152021@gmail.com>
2023-05-16 17:03:58 -0700

    Configure receiver side

--- Final Project - Public Key/client.py
+++ Final Project - Public Key/client.py
@@ -1,9 +1,17 @@
-import rsa
+import rsa,os,sys,hashlib,hmac
 
-publicKey, privateKey = rsa.newkeys(1024)
+#get the client private key to decrypt message
+with open(os.path.join(sys.path[0],"clientPrivateKey.pem"),"rb") as f:
+    clientPrivateKey = rsa.PrivateKey.load_pkcs1(f.read())
+    
+#get the data encrypted message from server
+with open(os.path.join(sys.path[0],"encryptedMessage.txt"),"rb") as f:
+    data = f.readlines()
+    encryptedMessage = data[0].strip()
+    mac = data[1]
 
-with open("clientPublicKey.pem","wb") as file:
-    file.write(publicKey.save_pkcs1("PEM"))
-
-with open("clientPrivateKey.pem","wb") as file:
-    file.write(privateKey.save_pkcs1("PEM"))
+#decrypt message 
+message = rsa.decrypt(encryptedMessage,clientPrivateKey)
+print(message.decode())
+#verify message integrity
+#mac = data[1].decode()
--- Final Project - Public Key/server.py
+++ Final Project - Public Key/server.py
@@ -1,1 +1,33 @@
-import rsa
+import rsa,os,sys,hashlib,hmac
+
+#read message to be sent to client
+#use os.path.join to open file from the same folder/directory
+with open(os.path.join(sys.path[0],"message.txt"),"r") as f:
+    message = f.read()
+
+#get the client public key
+with open(os.path.join(sys.path[0],"clientPublicKey.pem"),"rb") as f:
+    clientPublicKey = rsa.PublicKey.load_pkcs1(f.read())
+
+
+#encrypt message with client public key 
+encrypted_message = rsa.encrypt(message.encode(),clientPublicKey)
+
+#fucntion to compute HMAC
+def calc_digest(key,message):
+    key = bytes(key,'utf-8')
+    message = bytes(message,'utf-8')
+    dig = hmac.new(key,message,hashlib.sha256)
+    return dig.hexdigest()
+
+#compute an HMAC
+mac = calc_digest('GoneWithTheWind4600!',message)
+print(mac)
+
+#write encrypted message to file
+with open(os.path.join(sys.path[0],"encryptedMessage.txt"),"wb") as f:
+    f.write(encrypted_message)
+    
+#write HMAC to file
+with open(os.path.join(sys.path[0],"encryptedMessage.txt"),"a") as f:
+    f.write("\n"+mac)
